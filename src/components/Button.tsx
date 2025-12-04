0950ddd0f6367789543fdba2528eb040593591c2
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 15:37:10 +0100

    refactor: Make button text color brighter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import type { ButtonProps } from '../../types/Types'
 function Button({ label, type, destination }: ButtonProps){
     return(
         <a href={destination}>
-            <button type={type} className='primaryBtn sub-text'>
+            <button type={type} className='primaryBtn sub-text text-textStrong'>
                 {label}
             </button>
         </a>
