5660ffc85d26bc8e37fab05a6488f4f28c9d7ed6
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 10:04:59 +0100

    feat: Fix main container in the center of the screen and add margins to both sides

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Services from './sections/Services'
 function App() {
 
   return (
-    <main className='flex flex-col gap-20 md:gap-40'>
+    <main className='flex flex-col gap-20 md:gap-40 mx-6 md:mx-[60px] lg:mx-[120px]'>
      <Nav />
      <Hero />
      <Services />
