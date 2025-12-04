8a85475a5a7dc25b0bf87cef6f2eda02738975ae
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 10:42:51 +0100

    feat: Add and style card content

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,23 @@
 import type { CardProps } from '../../types/Types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Card({ icon, title, content }: CardProps){
     return (
-        <>
-            <p>
-                {icon}
-                {title}
+        <article className='flex flex-col p-6 gap-6 bg-[rgba(102,123,163,0.2)] rounded-xl'>
+            <div className='flex flex-col gap-2'>
+                <div className='p-4 w-14 h-14 rounded-full bg-[rgba(0,35,102,0.4)] border border-primaryColor flex items-center'>
+                    <FontAwesomeIcon icon={icon} size='lg' />
+                </div>
+
+                <h4>
+                    {title}
+                </h4>
+            </div>
+
+            <p className="sub-text">
                 {content}
             </p>
-        </>
+        </article>
     )
 }
 
