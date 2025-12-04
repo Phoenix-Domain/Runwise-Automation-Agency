2445d7ca9702dc7205a3783c7d1f3ab9c0258d40
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 09:58:46 +0100

    feat: Style badge according to design file specs

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,7 +2,7 @@ import type { BadgeProps } from '../../types/Types'
 
 function Badge ({ label }: BadgeProps){
     return(
-        <div>
+        <div className='py-4 px-6 rounded-full w-fit bg-[rgba(0,35,102,0.4)] border border-primaryColor'>
             {label}
         </div>
     )
