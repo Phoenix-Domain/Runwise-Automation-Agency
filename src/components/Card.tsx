54344faaca6bd890805a5d94d59134296ef72204
Batubo Victory <victorybatubo76@gmail.com>
2025-12-04 11:00:42 +0100

    feat: Conditionally render icons based on the type

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,11 +2,19 @@ import type { CardProps } from '../../types/Types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Card({ icon, title, content }: CardProps){
-       return (
+    // Function to check if icon is fontAwesome
+    const isFontAwesome = typeof icon === 'object' && 'iconName' in icon;
+
+    // Treat react icon as a variable, so it renders properly
+    const IconComponent = icon as React.ElementType;
+
+    return (
         <article className='flex flex-col p-6 gap-6 bg-[rgba(102,123,163,0.2)] rounded-xl'>
             <div className='flex flex-col gap-2'>
                 <div className='p-4 w-14 h-14 rounded-full bg-[rgba(0,35,102,0.4)] border border-primaryColor flex items-center'>
-                    <FontAwesomeIcon icon={icon} size='lg' />
+                    {
+                        isFontAwesome ? (<FontAwesomeIcon icon={icon} size='lg' />) : <IconComponent className='text-2xl' />
+                    }
                 </div>
 
                 <h4>
